9a47b82cc21d90b53bafffa7c7b73587cbf39a18
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-03-19 20:00:18 -0400

    Parsed next numbers
    Co-authored-by: kiyanpremani95@gmail.com <kiyanpremani95@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,13 +4,45 @@
 #include <cmath>
 
 
+int next_number(std::string& text) {
+	std::string n = "";
+
+	for (char c : text) {
+		if (isdigit(c)) {
+			n += c;
+		} else {
+			break;
+		}
+	}
+
+	text = text.substr(n.length());
+
+	return std::stoi(n);
+}
+
 int main() {
     std::string input;
 
-    std::cout << "Enter an equation: ";
+    std::cout << "Enter an expression: ";
     std::getline(std::cin, input);
 
-    std::cout << input << std::endl;
+    std::vector<int> numbers;
+
+    while (!input.empty()) {
+        numbers.push_back(next_number(input));
+
+        // There are still characters
+        if (!input.empty()) {
+            input = input.substr(1);
+        }
+    }
+
+    int sum = 0;
+    for (int n : numbers) {
+        sum += n;
+    }
+
+    std::cout << sum << std::endl;
 
     return 0;
 }
